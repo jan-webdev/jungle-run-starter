f4645e662a5a6a2efbdc269f244dd079ae98637d
Jan <jan.leon.voelker@gmail.com>
2020-10-29 14:42:11 +0100

    kommentierter starter code für später :).

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const game = new Game();
+const game = new Game();  //falls wir hier eine variable rein packen würden in die (), würden wir die im Constructor der Game class nutzen
 
 function preload() {
-  game.preloadGame();
+  game.preloadGame(); //holt alle Bilder und Kram, der in der Preload-Funktion der Class sind
 }
 function setup() {
   createCanvas(600, 400); //eine p5 method
   game.setupGame();
 }
-function draw() {
+function draw() {  // wird 60x pro sekunde aufgerufen, von sich aus!!
   clear(); // whipes the screen every 1/60th of a second, to make a moving instead of smearing effect
   game.drawGame();
 
--- js/player.js
+++ js/player.js
@@ -6,15 +6,28 @@ class Player {
     this.width = 100;
     this.x = 0;
     this.y = height - this.height;// alles wird ab oben links gezeichnet, daher diese rechnung, damit der character weder im boden steckt noch an der decke klebt
+    this.gravity = 0.2;
+    this.velocity = 0;
     //   }
   }
   drawPlayer() {
+    this.velocity += this.gravity;
+    this.y += this.velocity;
     // draw the player + jump logic with velocity
-    image(this.image, this.x, this.y);
+
+    // if player goes off the page
+    if (this.y >= height-this.height) {
+      this.y = height - this.height;
+    }
+    image(this.image, this.x, this.y); // Reihenfolge wichtig, erst Mathe checken, dann image laden, damit es nicht abhaut
   }
 
   // add a jump function
   jump() {
-    console.log("this will be the jump")
+    if (this.y == height - this.height) {
+         this.velocity = -10;
+    console.log("this will be the jump"); 
+    }
+
   }
 }
